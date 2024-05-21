b92a7b01415e6d6a56b3a93f61d71acf2117af2d
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2024-05-21 23:19:22 +0300

    launcher icons

--- launcher/src-tauri/src/main.rs
+++ launcher/src-tauri/src/main.rs
@@ -4,32 +4,17 @@
 use io::Result;
 use std::{io, process::Command, thread::sleep, time::Duration};
 
-fn run_command_in_console(command: &str, args: &[&str]) -> io::Result<()> {
-    // Create a new command for cmd.exe
+fn run_command_in_console(command: &str, args: &[&str])  {
     let mut cmd = Command::new("cmd");
 
-    // Add the /K flag to keep the console open and the command to execute
     cmd.arg("/K").arg(command).args(args);
 
-    // Spawn the command and wait for it to complete
-    let status = cmd.spawn()?;
-
-
-
-    Ok(())
+    let status = cmd.spawn();
 }
 
 #[tauri::command]
-fn runfile(name: &str) -> String {
+fn runfile(name: &str) {
     run_command_in_console("E:/Dev/priede/releases/0-1-0/priede_cli_0-1-0.exe", &[name]);
-    /* 
-    let mut command = Command::new("E:/Dev/priede/releases/0-1-0/priede_cli_0-1-0.exe");
-    command.arg(name);
-    let mut handle = command.spawn();
-    sleep(Duration::from_secs(5));
-    handle.unwrap().wait();
-    */
-    return "a".to_string();
 }
 
 fn main() {
