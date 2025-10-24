5c8768042cb35a60dd28159d3bdad44d4d16c45f
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2025-10-24 13:18:15 +0300

    add warning suppressor to priede.rs

--- build_gram.py
+++ build_gram.py
@@ -1,3 +1,12 @@
 import os
 os.chdir("./interpreter/src/hime")
 os.system('cmd /c "himecc.exe priede.gram -t rust"')
+with open("./priede.rs", "r", encoding="utf-8") as file:
+    existing_content = file.read()
+
+    # Combine the new text with the existing content
+    new_content = ("#![allow(dead_code)]\n"+ existing_content)
+
+    # Write the combined content back to the file
+    with open("./priede.rs", "w", encoding="utf-8-sig") as file:
+        file.write(new_content)
