89af6f7e0cfa91daa4933d05daef90c536b6616b
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2025-10-24 14:32:43 +0300

    improve parser error printing & add dependencies from github, not local directories

--- interpreter/src/parser.rs
+++ interpreter/src/parser.rs
@@ -1,39 +1,22 @@
-use celsium::compiletime_helper::{CompileTimeHelper, CompilerError};
+use celsium::compiletime_helper::{ CompileTimeHelper, CompilerError };
 use hime_redist::errors::{
     ParseError,
     ParseErrorDataTrait,
-    ParseErrorUnexpectedChar,
     ParseErrorUnexpectedToken,
 };
-
 
 fn unexpected_token_error(
     err: ParseErrorUnexpectedToken,
     compilehelper: &mut CompileTimeHelper
 ) -> CompilerError {
-    let mut err_str = err.to_string();
-    let expected_start = err.to_string().find("\"").unwrap();
-    let mut split = err_str.split_off(expected_start);
-    let _ = split.split_off(split.len() - 8);
-    let unexpected_token = split.split_off(1);
+    let unexpected_token = err.clone().value;
     let position = err.get_position();
 
-    let error = CompilerError{ message: format!("Neparedzēts simbols {}", unexpected_token), line: Some(position.line), file: compilehelper.source_file_paths[compilehelper.current_file].clone() };
-    crate::errors::parser_error(unexpected_token, err.get_position(), compilehelper);
-    return error;
-}
-fn unexpected_char_error(
-    err: ParseErrorUnexpectedChar,
-    compilehelper: &mut CompileTimeHelper
-) -> CompilerError {
-    let mut err_str = err.to_string();
-    let expected_start = err.to_string().find("'").unwrap();
-    let mut split = err_str.split_off(expected_start);
-    let _ = split.split_off(split.len() - 8);
-    let unexpected_token = split.split_off(1);
-    let position = err.get_position();
-
-    let error = CompilerError{ message: format!("Neparedzēts simbols {}", unexpected_token), line: Some(position.line), file: compilehelper.source_file_paths[compilehelper.current_file].clone() };
+    let error = CompilerError {
+        message: format!("Neparedzēts simbols {}", unexpected_token),
+        line: Some(position.line),
+        file: compilehelper.source_file_paths[compilehelper.current_file].clone(),
+    };
     crate::errors::parser_error(unexpected_token, err.get_position(), compilehelper);
     return error;
 }
@@ -44,16 +27,11 @@ pub fn parser_errors(
 ) -> Vec<CompilerError> {
     let mut result_errors = vec![];
     for parse_err in errors.clone() {
-        let error = match parse_err {
-            hime_redist::errors::ParseError::UnexpectedEndOfInput(_) => todo!(),
-            hime_redist::errors::ParseError::UnexpectedChar(err) =>
-                unexpected_char_error(err, compilehelper),
+        match parse_err {
             hime_redist::errors::ParseError::UnexpectedToken(err) =>
-                unexpected_token_error(err, compilehelper),
-            hime_redist::errors::ParseError::IncorrectUTF16NoLowSurrogate(_) => todo!(),
-            hime_redist::errors::ParseError::IncorrectUTF16NoHighSurrogate(_) => todo!(),
-        };
-        result_errors.push(error);
+                result_errors.push(unexpected_token_error(err, compilehelper)),
+            _ => (),
+        }
     }
     result_errors
 }
