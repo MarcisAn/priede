08f211cdb6b9bcaaba6162c281376e2500812075
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2026-03-13 08:40:14 +0200

    add explanation and step functions to wasm imports

--- build_wasm.py
+++ build_wasm.py
@@ -8,7 +8,10 @@ def build_wasm():
         existing_content = file.read()
 
         # Combine the new text with the existing content
-        new_content = "import { wasm_print, wasm_input, get_stumbrs_data, code_replace } from \"../priede.js\";\n" + existing_content
+        new_content = (
+            'import { wasm_print, wasm_input, get_stumbrs_data, code_replace, explain, step } from "../priede.js";\n/* eslint-disable */\n'
+ + existing_content
+        )
 
         # Write the combined content back to the file
         with open("./pkg/priede_wasm.js", 'w') as file:
@@ -19,6 +22,6 @@ def build_wasm():
         os.system('cmd /c  "robocopy  ./pkg ../web-editor-new/src/lib/pkg" /E /MOVE /NFL /NDL /NJH /NJS /nc /ns /np')
     else:
         print("wasm package moving to web-editor not implemented on other platforms")
-        
+
 if __name__ == "__main__":
     build_wasm()
